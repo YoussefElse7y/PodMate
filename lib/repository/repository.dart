21cbcf2d525dc442db8c8168c04acfbaf0ddd8d8
youssef <youssefele7y422@gmail.com>
2025-11-03 19:51:36 +0200

    Initial commit - PodMate Flutter App

---
+++ lib/repository/repository.dart
@@ -0,0 +1,75 @@
+// Copyright 2020 Ben Hills and the project contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:podcast_player/entities/episode.dart';
+import 'package:podcast_player/entities/podcast.dart';
+import 'package:podcast_player/entities/transcript.dart';
+import 'package:podcast_player/state/episode_state.dart';
+
+/// An abstract class that represent the actions supported by the chosen
+/// database or storage implementation.
+abstract class Repository {
+  /// General
+  Future<void> close();
+
+  /// Podcasts
+  Future<Podcast?> findPodcastById(num id);
+
+  Future<Podcast?> findPodcastByGuid(String guid);
+
+  Future<Podcast> savePodcast(Podcast podcast, {bool withEpisodes = true});
+
+  Future<void> deletePodcast(Podcast podcast);
+
+  Future<List<Podcast>> subscriptions();
+
+  /// Episodes
+  Future<List<Episode>> findAllEpisodes();
+
+  Future<Episode?> findEpisodeById(int id);
+
+  Future<Episode?> findEpisodeByGuid(String guid);
+
+  Future<List<Episode?>> findEpisodesByPodcastGuid(
+    String pguid, {
+    PodcastEpisodeFilter filter = PodcastEpisodeFilter.none,
+    PodcastEpisodeSort sort = PodcastEpisodeSort.none,
+  });
+
+  Future<Episode?> findEpisodeByTaskId(String taskId);
+
+  Future<Episode?> findNextPlayableEpisode(Episode episode);
+
+  Future<Episode> saveEpisode(Episode episode, [bool updateIfSame = false]);
+
+  Future<List<Episode>> saveEpisodes(
+    List<Episode> episodes, [
+    bool updateIfSame = false,
+  ]);
+
+  Future<void> deleteEpisode(Episode episode);
+
+  Future<void> deleteEpisodes(List<Episode> episodes);
+
+  Future<List<Episode>> findDownloadsByPodcastGuid(String pguid);
+
+  Future<List<Episode>> findDownloads();
+
+  Future<Transcript?> findTranscriptById(int id);
+
+  Future<Transcript> saveTranscript(Transcript transcript);
+
+  Future<void> deleteTranscriptById(int id);
+
+  Future<void> deleteTranscriptsById(List<int> id);
+
+  /// Queue
+  Future<void> saveQueue(List<Episode> episodes);
+
+  Future<List<Episode>> loadQueue();
+
+  /// Event listeners
+  late Stream<Podcast> podcastListener;
+  late Stream<EpisodeState> episodeListener;
+}
---
+++ lib/services/settings/settings_service.dart
@@ -0,0 +1,69 @@
+// Copyright 2020 Ben Hills and the project contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:podcast_player/entities/app_settings.dart';
+
+abstract class SettingsService {
+  AppSettings? get settings;
+
+  set settings(AppSettings? settings);
+
+  String get theme;
+
+  set theme(String mode);
+
+  bool get markDeletedEpisodesAsPlayed;
+
+  set markDeletedEpisodesAsPlayed(bool value);
+
+  bool get deleteDownloadedPlayedEpisodes;
+
+  set deleteDownloadedPlayedEpisodes(bool value);
+
+  bool get storeDownloadsSDCard;
+
+  set storeDownloadsSDCard(bool value);
+
+  set playbackSpeed(double playbackSpeed);
+
+  double get playbackSpeed;
+
+  set searchProvider(String provider);
+
+  String get searchProvider;
+
+  set externalLinkConsent(bool consent);
+
+  bool get externalLinkConsent;
+
+  set autoOpenNowPlaying(bool autoOpenNowPlaying);
+
+  bool get autoOpenNowPlaying;
+
+  set showFunding(bool show);
+
+  bool get showFunding;
+
+  set autoUpdateEpisodePeriod(int period);
+
+  int get autoUpdateEpisodePeriod;
+
+  set trimSilence(bool trim);
+
+  bool get trimSilence;
+
+  set volumeBoost(bool boost);
+
+  bool get volumeBoost;
+
+  set layoutMode(int mode);
+
+  int get layoutMode;
+
+  set autoPlay(bool autoPlay);
+
+  bool get autoPlay;
+
+  Stream<String> get settingsListener;
+}
